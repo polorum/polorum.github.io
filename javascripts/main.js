75d4e6eaefc08d97b5b7707d49801b0c7c57c953
JennaLH <jenna.howe@live.co.uk>
2015-01-22 14:13:05 +0000

    Make menu stick to top of screen on scrolling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(e) {
+    var menuTopPos = $('#menu').offset().top;
+
+    var stickToTop = function(){
+        var winScrollTop = $(window).scrollTop();
+                  
+        if (winScrollTop >= menuTopPos) {
+                $('#menu').addClass('stick_top');
+        } else {
+                $('#menu').removeClass('stick_top');  
+        }
+        
+    };
+
+    stickToTop();
+
+    $(window).scroll(function() {
+            stickToTop();
+    });
+});
